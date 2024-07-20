21f80057fe5045c37581091be62437f93b2f54bd
notmiguelalves <not.miguel.alves@proton.me>
2024-07-20 14:35:59 +0100

    add slog with proper levels

--- main.go
+++ main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"context"
+	"log/slog"
+	"os"
 
 	"github.com/notmiguelalves/anypipe/pkg/dockerutils"
 )
 
 func main() {
 	ctx := context.Background()
-	du, err := dockerutils.New(ctx)
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+
+	du, err := dockerutils.New(ctx, logger)
 	if err != nil {
 		panic(err)
 	}
--- pkg/dockerutils/dockerutils.go
+++ pkg/dockerutils/dockerutils.go
@@ -3,8 +3,9 @@ package dockerutils
 import (
 	"context"
 	"fmt"
-	"io"
+	"log/slog"
 	"os"
+	"strings"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
@@ -13,24 +14,31 @@ import (
 
 type DockerUtils struct {
 	ctx               context.Context
+	logger            *slog.Logger
 	dockerClient      *client.Client
 	spawnedContainers []*Container
 }
 
-func New(ctx context.Context) (*DockerUtils, error) {
+func New(ctx context.Context, logger *slog.Logger) (*DockerUtils, error) {
 	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
 	if err != nil {
+		logger.Error(fmt.Sprintf("failed to initialize docker client: %s", err.Error()))
 		return nil, err
 	}
 
 	return &DockerUtils{
 		ctx:          ctx,
 		dockerClient: cli,
+		logger:       logger,
 	}, nil
 }
 
 func (du *DockerUtils) Close() error {
+	du.logger.Info("cleaning up spawned containers")
+
 	for _, c := range du.spawnedContainers {
+		du.logger.Info(fmt.Sprintf("going to cleanup %s", c.id))
+
 		err := du.dockerClient.ContainerRemove(du.ctx, c.id, container.RemoveOptions{
 			RemoveVolumes: false,
 			RemoveLinks:   false,
@@ -38,7 +46,7 @@ func (du *DockerUtils) Close() error {
 		})
 
 		if err != nil {
-			fmt.Printf("failed to remove container %s: %s\n", c.id, err.Error())
+			du.logger.Error(fmt.Sprintf("failed to cleanup container %s: %s", c.id, err.Error()))
 		}
 	}
 
@@ -52,58 +60,55 @@ func (du *DockerUtils) CreateContainer(image string) (*Container, error) {
 		Tty:   false,
 	}, nil, nil, nil, "")
 	if err != nil {
+		du.logger.Error(fmt.Sprintf("failed to create container from '%s' : %s", image, err.Error()))
 		return nil, err
 	}
 
 	c := Container{id: resp.ID}
 	du.spawnedContainers = append(du.spawnedContainers, &c)
 
+	du.logger.Info(fmt.Sprintf("going to start container %s created from image %s", c.id, image))
 	if err := du.dockerClient.ContainerStart(du.ctx, c.id, container.StartOptions{}); err != nil {
+		du.logger.Error(fmt.Sprintf("failed to start container '%s' : %s", c.id, err.Error()))
 		return &c, err
 	}
 
-	// TODO : poll container state and only return from this function when the container is fully up and running ?
-
+	du.logger.Info(fmt.Sprintf("started container %s", c.id))
 	return &c, nil
 }
 
 func (du *DockerUtils) Exec(c *Container, cmd []string) error {
+	du.logger.Info(fmt.Sprintf("going to execute %s on container %s", strings.Join(cmd, " "), c.id))
+
 	resp, err := du.dockerClient.ContainerExecCreate(du.ctx, c.id, container.ExecOptions{Cmd: cmd, Detach: false, AttachStderr: true, AttachStdout: true, WorkingDir: "/home"})
 	if err != nil {
+		du.logger.Error("failed to create exec operation on container %s : %s", c.id, err.Error())
 		return err
 	}
 
 	attachResp, err := du.dockerClient.ContainerExecAttach(du.ctx, resp.ID, container.ExecAttachOptions{})
 	if err != nil {
+		du.logger.Error("failed to attach exec operation to container %s : %s", c.id, err.Error())
 		return err
 	}
 	defer attachResp.Close()
 
 	err = du.dockerClient.ContainerExecStart(du.ctx, resp.ID, container.ExecStartOptions{})
 	if err != nil {
+		du.logger.Error("failed to start exec operation on container %s : %s", c.id, err.Error())
 		return err
 	}
 
-	errChan := make(chan error, 1)
-
-	go func() {
-		_, errorInContainer := stdcopy.StdCopy(os.Stdout, os.Stderr, attachResp.Reader)
-		errChan <- errorInContainer
-	}()
-
-	go func() {
-		_, err := io.Copy(attachResp.Conn, os.Stdin)
-		errChan <- err
-	}()
-
-	select {
-	case errorInContainer := <-errChan:
-		if errorInContainer != nil {
-			fmt.Printf("dtools exec error: %s\n", errorInContainer.Error())
-			os.Exit(-1)
-		}
-		return nil
+	_, err = stdcopy.StdCopy(os.Stdout, os.Stderr, attachResp.Reader)
+	if err != nil {
+		du.logger.Error(fmt.Sprintf("failed to fetch container stdout/stderr from container %s : %s", c.id, err.Error()))
+		return err
 	}
+
+	// TODO @Miguel : instead of writing to stdout and stderr, function should receive two params where to write
+	// TODO @Miguel : fetch and return exit code as well
+
+	return nil
 }
 
 type Container struct {
