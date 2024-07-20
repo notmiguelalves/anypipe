678815673f05eba9deb2cd2f5e9612c2694f49f7
notmiguelalves <not.miguel.alves@proton.me>
2024-07-20 17:48:57 +0100

    refactor copy funcs

--- pkg/dockerutils/docker.go
+++ pkg/dockerutils/docker.go
@@ -1,20 +1,18 @@
 package dockerutils
 
 import (
-	"archive/tar"
-	"bytes"
 	"context"
 	"fmt"
 	"io"
 	"log/slog"
 	"os"
-	"path/filepath"
 	"strings"
 
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/notmiguelalves/anypipe/pkg/utils"
 )
 
 type DockerUtils struct {
@@ -150,52 +148,13 @@ func (du *DockerUtils) Exec(c *Container, cmd string) error {
 }
 
 func (du *DockerUtils) CopyTo(c *Container, srcPath, dstPath string) error {
-	var buf bytes.Buffer
-	// zr := gzip.NewWriter(&buf)
-	// defer zr.Close()
-	tw := tar.NewWriter(&buf)
-	defer tw.Close()
-
-	err := filepath.Walk(srcPath, func(file string, fi os.FileInfo, err error) error {
-		if err != nil {
-			return err
-		}
-
-		header, err := tar.FileInfoHeader(fi, file)
-		if err != nil {
-			return err
-		}
-
-		relPath, err := filepath.Rel(srcPath, file)
-		if err != nil {
-			return err
-		}
-		header.Name = filepath.ToSlash(relPath)
-
-		if err := tw.WriteHeader(header); err != nil {
-			return err
-		}
-
-		if !fi.IsDir() {
-			data, err := os.Open(file)
-			if err != nil {
-				return err
-			}
-			defer data.Close()
-
-			_, err = io.Copy(tw, data)
-			if err != nil {
-				return err
-			}
-		}
-		return nil
-	})
-
+	buf, err := utils.Tar(srcPath)
 	if err != nil {
+		du.logger.Error(fmt.Sprintf("failed to tar %s : %s", srcPath, err.Error()))
 		return err
 	}
 
-	err = du.dockerClient.CopyToContainer(du.ctx, c.id, dstPath, &buf, container.CopyToContainerOptions{})
+	err = du.dockerClient.CopyToContainer(du.ctx, c.id, dstPath, buf, container.CopyToContainerOptions{})
 	if err != nil {
 		return err
 	}
@@ -210,44 +169,11 @@ func (du *DockerUtils) CopyFrom(c *Container, srcPath, dstPath string) error {
 		return err
 	}
 
-	tr := tar.NewReader(rc)
-
-	for {
-		header, err := tr.Next()
-
-		switch {
-		case err == io.EOF:
-			return nil
-
-		case err != nil:
-			return err
-
-		case header == nil:
-			continue
-		}
-
-		target := filepath.Join(dstPath, header.Name)
-
-		switch header.Typeflag {
-		case tar.TypeDir:
-			if _, err := os.Stat(target); err != nil {
-				if err := os.MkdirAll(target, 0755); err != nil {
-					return err
-				}
-			}
-
-		case tar.TypeReg:
-			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
-			if err != nil {
-				return err
-			}
-
-			if _, err := io.Copy(f, tr); err != nil {
-				return err
-			}
-
-			// defering would cause each file close to wait until all operations have completed.
-			_ = f.Close()
-		}
+	err = utils.Untar(rc, dstPath)
+	if err != nil {
+		du.logger.Error(fmt.Sprintf("failed to untar %s : %s", srcPath, err.Error()))
+		return err
 	}
+
+	return nil
 }
---
+++ pkg/utils/utils.go
@@ -0,0 +1,97 @@
+package utils
+
+import (
+	"archive/tar"
+	"bytes"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func Tar(path string) (*bytes.Buffer, error) {
+	var buf bytes.Buffer
+	// zr := gzip.NewWriter(&buf)
+	// defer zr.Close()
+	tw := tar.NewWriter(&buf)
+	defer tw.Close()
+
+	err := filepath.Walk(path, func(file string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := tar.FileInfoHeader(fi, file)
+		if err != nil {
+			return err
+		}
+
+		relPath, err := filepath.Rel(path, file)
+		if err != nil {
+			return err
+		}
+		header.Name = filepath.ToSlash(relPath)
+
+		if err := tw.WriteHeader(header); err != nil {
+			return err
+		}
+
+		if !fi.IsDir() {
+			data, err := os.Open(file)
+			if err != nil {
+				return err
+			}
+			defer data.Close()
+
+			_, err = io.Copy(tw, data)
+			if err != nil {
+				return err
+			}
+		}
+		return nil
+	})
+
+	return &buf, err
+}
+
+func Untar(rc io.ReadCloser, dstPath string) error {
+	tr := tar.NewReader(rc)
+
+	for {
+		header, err := tr.Next()
+
+		switch {
+		case err == io.EOF:
+			return nil
+
+		case err != nil:
+			return err
+
+		case header == nil:
+			continue
+		}
+
+		target := filepath.Join(dstPath, header.Name)
+
+		switch header.Typeflag {
+		case tar.TypeDir:
+			if _, err := os.Stat(target); err != nil {
+				if err := os.MkdirAll(target, 0755); err != nil {
+					return err
+				}
+			}
+
+		case tar.TypeReg:
+			f, err := os.OpenFile(target, os.O_CREATE|os.O_RDWR, os.FileMode(header.Mode))
+			if err != nil {
+				return err
+			}
+
+			if _, err := io.Copy(f, tr); err != nil {
+				return err
+			}
+
+			// defering would cause each file close to wait until all operations have completed.
+			_ = f.Close()
+		}
+	}
+}
