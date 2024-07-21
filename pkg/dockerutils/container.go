512a9b6e33588166c97280c7a54fa88309466577
notmiguelalves <not.miguel.alves@proton.me>
2024-07-21 17:06:14 +0100

    add comments

--- pkg/dockerutils/container.go
+++ pkg/dockerutils/container.go
@@ -14,6 +14,7 @@ func sanitizeEnvKey(key string) string {
 	return strings.TrimSpace(strings.ReplaceAll(key, " ", ""))
 }
 
+// returns a list of KEY=VALUE environment variable bindings for the container
 func (c *Container) Env() []string {
 	env := []string{}
 	for key, value := range c.env {
@@ -23,10 +24,12 @@ func (c *Container) Env() []string {
 	return env
 }
 
+// adds and environment variable with KEY and VALUE to container
 func (c *Container) AddEnv(key, value string) {
 	c.env[sanitizeEnvKey(key)] = value
 }
 
+// removes environment variable with KEY if it exists in container
 func (c *Container) RemoveEnv(key string) {
 	delete(c.env, sanitizeEnvKey(key))
 }
